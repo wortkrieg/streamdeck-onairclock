33648309fcb670347b32b95fe5e230131c196a48
Marc Herrmann <marc@marc.fail>
2022-12-25 14:14:06 +0100

    improved deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@
 pkill -x "Stream Deck"
 
 # Simply remove the old folder in streamdeck's plugin folder and replace by current one. Has to be replaced by own path.
-rm -r "/Users/marc/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
+rm -r "$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
 
-cp -a fail.marc.onairclock.sdPlugin/ "/Users/marc/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
+cp -a fail.marc.onairclock.sdPlugin/ "$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
 
 #launch stream deck app
 open -a "Elgato Stream Deck"
