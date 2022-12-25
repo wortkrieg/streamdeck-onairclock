89cfc685c4b14b2d90f79dadaeb40ad0cf5c6735
Marc Herrmann <marc@marc.fail>
2022-12-25 12:02:40 +0100

    kill and relaunch streamdeck app when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# Simply remove the old folder in streamdeck's plugin folder and replace by current one. Has to be replaced by own path.
+#this script works mac only!
 
+# Shoot stream deck app
+pkill -x "Stream Deck"
+
+# Simply remove the old folder in streamdeck's plugin folder and replace by current one. Has to be replaced by own path.
 rm -r "/Users/marc/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
 
 cp -a fail.marc.onairclock.sdPlugin/ "/Users/marc/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
+
+#launch stream deck app
+open -a "Elgato Stream Deck"
