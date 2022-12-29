e98a12e87d663864b850170cb04ba2573655b835
Marc Herrmann <marc@marc.fail>
2022-12-29 13:21:20 +0100

    1 sec grace time so make sure sd is closed

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@
 # Shoot stream deck app
 pkill -x "Stream Deck"
 
+sleep 1  # Waits 1 second.
+
 # Simply remove the old folder in streamdeck's plugin folder and replace by current one. Has to be replaced by own path.
 rm -r "$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins/fail.marc.onairclock.sdPlugin"
 
